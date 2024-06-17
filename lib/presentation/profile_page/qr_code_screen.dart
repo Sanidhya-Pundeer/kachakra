8a495ff1c9fad810ab854b1e8f012ac722c0249f
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-06-17 15:58:13 +0530

    scanner screen updated

--- lib/presentation/profile_page/qr_code_screen.dart
+++ lib/presentation/profile_page/qr_code_screen.dart
@@ -16,13 +16,93 @@ class _CodeScreenState extends State<CodeScreen> {
         padding: EdgeInsets.all(10),
         child: Column(
           children: [
-            Text('First Middle LastName'),
-            Image.asset(
-              'assets/images/qr_code.png',
-              height: 250,
-              width: 250,
-              alignment: Alignment.center,
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                width: 390,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 206, 238, 220),
+                    borderRadius: BorderRadius.all(Radius.circular(7))),
+                child: Column(
+                  children: [
+                    Text(
+                      'First Middle LastName',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(3))),
+                      child: Image.asset(
+                        'assets/images/qr_code.png',
+                        height: 250,
+                        width: 250,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text('Scan to pay or get user details'),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 105,
+                        ),
+                        Text('User ID: 35286832598'),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        IconButton(onPressed: () => {}, icon: Icon(Icons.copy))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
+            SizedBox(
+              height: 35,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                ElevatedButton(
+                    onPressed: () => {},
+                    child: Row(
+                      children: [
+                        Icon(Icons.qr_code_scanner_rounded),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text('Open in Scanner'),
+                      ],
+                    )),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () => {},
+                    child: Row(
+                      children: [
+                        Icon(Icons.share),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text('Share QR Code'),
+                      ],
+                    )),
+              ],
+            )
           ],
         ),
       ),
